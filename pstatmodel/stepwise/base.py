88ec1305acce026e5bfc437900ddf1400b82964b
Gerardo Rivera Tello <gerardo_art@me.com>
2021-04-28 23:49:59 -0500

    Fix R drop condition

--- pstatmodel/stepwise/base.py
+++ pstatmodel/stepwise/base.py
@@ -10,7 +10,7 @@ def stepwise_selection(
     X,
     y,
     initial_list=[],
-    threshold_in=0.1,
+    threshold_in=0.05,
     threshold_out=0.1,
     verbose=True,
     max_vars=12,
@@ -33,8 +33,9 @@ def stepwise_selection(
     included_pvals = []
     included_rvals = []
     # min_pvalue = 0.1
-    # lower = False
+    lower = False
     rcond = False
+    dropped = False
     #     over = False
     if np.isnan(y).any():
         return [], np.nan, np.nan
@@ -65,6 +66,7 @@ def stepwise_selection(
         #         print(f"{best_pval=} // {worst_pval=}")
         if worst_pval > threshold_out:
             changed = True
+            dropped = True
             worst_feature = pvalues.index[pvalues.argmax()]
             _idx = included.index(worst_feature)
             included_pvals.pop(_idx)
@@ -75,10 +77,11 @@ def stepwise_selection(
 
         if len(included) >= min_vars and len(included) <= max_vars and not changed:
             break
-        elif len(included) > max_vars:
+        elif len(included) > max_vars and not dropped:
             psize = 999
             included_pvals = np.array(included_pvals)
-            while psize > max_vars:
+            idem = False
+            while psize > max_vars or idem:
                 threshold_in = np.round(max([0.01, threshold_in - 0.01]), decimals=2)
                 threshold_in_next = np.round(
                     max([0.01, threshold_in - 0.01]), decimals=2
@@ -87,18 +90,31 @@ def stepwise_selection(
                 mask = included_pvals < threshold_in
                 mask_next = included_pvals < threshold_in_next
 
-                psize = mask.sum()
-                psize_next = mask_next.sum()
+                if False in mask:
+                    psize = mask[: np.where(mask == False)[0][0]].sum()
+                else:
+                    psize = mask.sum()
+                if False in mask_next:
+                    psize_next = mask_next[: np.where(mask_next == False)[0][0]].sum()
+                else:
+                    psize_next = mask_next.sum()
 
-                if psize == psize_next and threshold_in != 0.01:
+                if psize == psize_next and threshold_in != 0.01 and psize != 0:
+                    print(f"{psize = }, {threshold_in = }")
+                    print(f"{psize_next = }, {threshold_in_next = }")
+                    idem = True
                     continue
+                else:
+                    idem = False
                 if psize >= min_vars and psize <= max_vars:
+                    print(f"break: {psize = }, {threshold_in = }")
                     pass
                 elif psize < min_vars and threshold_in != 0.1:
                     included_rvals = np.array(included_rvals)
                     mask = included_rvals < 0.9
                     threshold_in = np.round(min([0.1, threshold_in + 0.01]), decimals=2)
                     rcond = True
+                    print("breaking on R condition")
                 elif threshold_in == 0.01 or threshold_in == 0.1:
                     break
                 else:
@@ -111,27 +127,29 @@ def stepwise_selection(
                 included = included[: mfalse[0]]
                 model = sm.OLS(y, sm.add_constant(X[included])).fit()
             break
+        elif dropped:
+            if len(included) > max_vars and threshold_in != 0.01 and not lower:
+                threshold_in = np.round(max([0.01, threshold_in - 0.01]), decimals=2)
+                if verbose:
+                    print(f"Upped threshold_in value to {threshold_in}")
+                included = []
+                changed = True
 
-        #     threshold_in = np.round(max([0.01, threshold_in - 0.01]), decimals=2)
-        #     if verbose:
-        #         print(f"Upped threshold_in value to {threshold_in}")
-        #     included = []
-        #     changed = True
-
-        # if len(included) >= min_vars and lower:
-        #     if model.rsquared ** (0.5) > 0.9:
-        #         changed = False
-        #         if verbose:
-        #             print("Breaking condition met: R value over 0.9")
+            if len(included) >= min_vars and lower:
+                if model.rsquared ** (0.5) > 0.9:
+                    changed = False
+                    if verbose:
+                        print("Breaking condition met: R value over 0.9")
 
         if not changed:
             #             break
-            # if len(included) < min_vars and threshold_in != 0.1:
-            #     threshold_in = np.round(min([0.1, threshold_in + 0.01]), decimals=2)
-            #     if verbose:
-            #         print(f"Dropped threshold_in value to {threshold_in}")
-            #     included = []
-            #     lower = True
-            # else:
-            break
+            if len(included) < min_vars and threshold_in != 0.1:
+                threshold_in = np.round(min([0.1, threshold_in + 0.01]), decimals=2)
+                if verbose:
+                    print(f"Dropped threshold_in value to {threshold_in}")
+                included = []
+                lower = True
+            else:
+                break
+    print(included)
     return included, model, threshold_in
