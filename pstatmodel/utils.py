69ecdea74b77b265d9042814332cdb2279c1c254
Gerardo Rivera Tello <gerardo_art@me.com>
2021-05-10 18:48:32 -0500

    Return column names with months appended

--- pstatmodel/utils.py
+++ pstatmodel/utils.py
@@ -9,10 +9,29 @@ def shift_predictor(
     fyear: int = 2017,
 ):
     _collection = []
+    months = [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Setiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre",
+    ]
     for year in range(iyear, fyear):
         _collection.append(
             table.query(
                 f"(time>'{year -1 }-{init_month}-01')&(time<'{year}-{init_month}-01')"
             )[predictor].reset_index(drop=True)
         )
-    return pd.concat(_collection, axis=1).T.reset_index(drop=True)
+    result = pd.concat(_collection, axis=1).T.reset_index(drop=True)
+    shiftIndex = int(init_month) - 1
+    months_shifted = months[shiftIndex:] + months[:shiftIndex]
+    column_names = [f"{predictor}_{month}" for month in months_shifted]
+    result.columns = column_names
+    return result
