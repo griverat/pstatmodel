2f0516556d4659ca7655177e69968369c1f7ff46
Gerardo Rivera Tello <gerardo_art@me.com>
2021-05-11 20:53:26 -0500

    Add test for new functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,3 +10,15 @@ expected = pd.read_csv("tests/data/e_reformat.txt", sep=";")
 def testShiftPredictor():
     result = utils.shift_predictor(ecData, "E", "08")
     np.array_equal(result, expected)
+
+
+def testVariableFetcher():
+    for predictor, pargs in utils.DATA_CONTAINTER.items():
+        if pargs["format"] == "long":
+            raw_data = utils.parse_fwf(**pargs)
+        elif pargs["format"] == "wide":
+            raw_data = utils.wide_to_long(**pargs)
+        else:
+            raw_data = None
+        assert predictor == pargs["name"]
+        assert raw_data is not None
