4635e62d28bac797be4013afb988694112e0ffc3
Gerardo Rivera Tello <gerardo_art@me.com>
2021-05-18 20:58:39 -0500

    Make dataclasses available in the main namespace

--- pstatmodel/__init__.py
+++ pstatmodel/__init__.py
@@ -1,7 +1,8 @@
 from . import utils
 from ._version import get_versions
+from .variable import ModelVariables, PredictorVariable
 
 __version__ = get_versions()["version"]
 del get_versions
 
-__all__ = ["utils"]
+__all__ = ["utils", "ModelVariables", "PredictorVariable"]
