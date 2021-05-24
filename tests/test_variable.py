081f41a0a4125b02f29b719d4aa971b61e17d138
Gerardo Rivera Tello <gerardo_art@me.com>
2021-05-24 16:36:16 -0500

    Write test for dataframe ingest into dataclass

--- tests/test_variable.py
+++ tests/test_variable.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pytest
 
 from pstatmodel import ModelVariables, PredictorVariable
-from pstatmodel.utils import DATA_CONTAINTER
+from pstatmodel.utils import DATA_CONTAINTER, monthResampler
 
 
 @pytest.mark.parametrize(
@@ -11,6 +11,71 @@ from pstatmodel.utils import DATA_CONTAINTER
 def test_Variables(predictor, source_data):
     Variable = PredictorVariable(predictor, **source_data)
     assert Variable.raw_data is not None
+
+
+def month_names(name, seasons=False):
+    if seasons:
+        return [
+            f"{name}_JAS",
+            f"{name}_ASO",
+            f"{name}_SON",
+            f"{name}_OND",
+            f"{name}_NDJ",
+            f"{name}_DJF",
+            f"{name}_JFM",
+            f"{name}_FMA",
+            f"{name}_MAM",
+            f"{name}_AMJ",
+            f"{name}_MJJ",
+            f"{name}_JJA",
+        ]
+
+    else:
+        return [
+            f"{name}_Agosto",
+            f"{name}_Setiembre",
+            f"{name}_Octubre",
+            f"{name}_Noviembre",
+            f"{name}_Diciembre",
+            f"{name}_Enero",
+            f"{name}_Febrero",
+            f"{name}_Marzo",
+            f"{name}_Abril",
+            f"{name}_Mayo",
+            f"{name}_Junio",
+            f"{name}_Julio",
+        ]
+
+
+@pytest.mark.parametrize(
+    "predictor,source_data", DATA_CONTAINTER.items(), ids=DATA_CONTAINTER.keys()
+)
+def test_ShiftVariables(predictor, source_data):
+    Variable = PredictorVariable(predictor, **source_data)
+    Variable.shiftData(init_month="08", fyear=2022)
+    if isinstance(Variable.shifted_data, list):
+        for df, df_shifted in zip(Variable.shifted_data, Variable.raw_data):
+            df_shifted = monthResampler(df_shifted)
+            expected = (
+                df_shifted.query("(time>'2005-08-01')&(time<'2006-08-01')")
+                .copy()
+                .iloc[:, 1]
+            )
+            expected.index = month_names(expected.name, Variable.use_seasons)
+            expected.name = 2006
+            pd.testing.assert_series_equal(df.loc[2006], expected.loc[df.columns])
+    else:
+        expected = (
+            monthResampler(Variable.raw_data)
+            .query("(time>'2005-08-01')&(time<'2006-08-01')")
+            .copy()
+            .iloc[:, 1]
+        )
+        expected.index = month_names(expected.name, Variable.use_seasons)
+        expected.name = 2006
+        pd.testing.assert_series_equal(
+            Variable.shifted_data.loc[2006], expected.loc[Variable.shifted_data.columns]
+        )
 
 
 @pytest.mark.parametrize(
